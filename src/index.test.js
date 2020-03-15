b7096e73596476c1b14304dd5018092f58c280c1
ajiron <ajiron@liveperson.com>
2020-03-15 01:10:58 -0700

    wip

--- src/index.test.js
+++ src/index.test.js
@@ -9,6 +9,10 @@ const GH_API = 'https://api.github.com';
 describe('getUncoveredLines', () => {
   beforeEach(() => {
     jest.resetModules();
+    nock.cleanAll();
+  });
+  afterAll(() => {
+    nock.cleanAll();
   });
 
   it('should return an empty array when no coverageMap is in testResults', async () => {
